75272f63605168c12eddfa2ec4e9535f5ba8a65f
Christopher Toledo <uberscientist@gmail.com>
2012-07-06 02:01:11 -0700

    Downloads all originals for a username

--- app.js
+++ app.js
@@ -1,32 +1,49 @@
 var request = require('request');
 var fs = require('fs');
 
-var user = {
-  ids: [{user: 'zincink', skip: 0}]
-}
+var username = 'yipyip';
+var offset = 0;
 
-var options = {
-  uri: 'http://canv.as/public_api/users/',
-  host: 'http://canv.as',
-  path: '/public_api/users/',
-  method: 'POST',
-  body: JSON.stringify(user)
-}
 
-request(options, function(err, res, body){
-  var body = JSON.parse(body);
-  console.log(body);
+function getImages(offset){
 
-  body.users[0].posts.map(function(post){
+  var offset_json = {
+    ids: [{user: username, skip: offset}]
+  }
 
-      request(post.api_url, function(err, res, body){
-        var body = JSON.parse(body);
-        if(typeof(body.reply_content.original) !== 'undefined'){
-          var orig = body.reply_content.original.url;
-          var ext = orig.slice(-4, orig.length);
-          request(orig).pipe(fs.createWriteStream(body.id + ext))
-        }
-      });
+  var options = {
+    uri: 'http://canv.as/public_api/users/',
+    host: 'http://canv.as',
+    path: '/public_api/users/',
+    method: 'POST',
+    body: JSON.stringify(offset_json)
+  }
 
+  request(options, function(err, res, body){
+    var body = JSON.parse(body);
+
+    var posts = body.users[0].posts;
+
+    //Increase our offset length for our next loop
+    if(posts.length > 0){
+      offset += 10;
+      getImages(offset);
+    }
+
+
+    posts.map(function(post){
+
+        request(post.api_url, function(err, res, body){
+          var body = JSON.parse(body);
+          if(typeof(body.reply_content.original) !== 'undefined'){
+            var orig = body.reply_content.original.url;
+            var ext = orig.slice(-4, orig.length);
+            request(orig).pipe(fs.createWriteStream(body.id + ext))
+          }
+        });
+
+    });
   });
-});
+}
+
+getImages(offset);
