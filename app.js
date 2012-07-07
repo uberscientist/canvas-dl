0de735b4910fae1f7bfd574390b9b91ac69ef4eb
Christopher Toledo <uberscientist@gmail.com>
2012-07-07 01:13:55 -0700

    An experiment with rate limiting

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var request = require('request');
 var fs = require('fs');
 
-var username = 'yipyip';
+var username = 'jiakko';
 var offset = 0;
 
 
@@ -22,27 +22,33 @@ function getImages(offset){
   request(options, function(err, res, body){
     var body = JSON.parse(body);
 
+    //console.log(body);
     var posts = body.users[0].posts;
 
     //Increase our offset length for our next loop
-    if(posts.length > 0){
+    if(posts.length == 10){
       offset += 10;
-      getImages(offset);
+      setTimeout(getImages(offset), 1000);
     }
 
+    var c = 0;
+    var limiting = setInterval(function(){
 
-    posts.map(function(post){
+      request(posts[c].api_url, function(err, res, body){
+        var body = JSON.parse(body);
+        console.log(body);
+        if(typeof(body.reply_content.original) !== 'undefined'){
+          var orig = body.reply_content.original.url;
+          var ext = orig.slice(-4, orig.length);
+          request(orig).pipe(fs.createWriteStream('./pics'+ body.id + ext))
+        }
+      });
 
-        request(post.api_url, function(err, res, body){
-          var body = JSON.parse(body);
-          if(typeof(body.reply_content.original) !== 'undefined'){
-            var orig = body.reply_content.original.url;
-            var ext = orig.slice(-4, orig.length);
-            request(orig).pipe(fs.createWriteStream(body.id + ext))
-          }
-        });
+      c++;
+      if(c == posts.length) clearInterval(limiting);
+    }, 1000);
 
-    });
+
   });
 }
 
